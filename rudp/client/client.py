074e7cd62e8181a5b50b76ef9fb37545a1cdd873
TheAlchemist1307 <nishitrchouhan@gmail.com>
2021-04-16 01:04:39 +0530

    Added filename input

--- rudp/client/client.py
+++ rudp/client/client.py
@@ -1,24 +1,33 @@
 import sys
+import os
 sys.path.append('../')
 from protocol_files.rudp_copy_protocol import RUDP
-r = RUDP("127.0.0.1",9001,False)
-r.connect_sock("127.0.0.1",9002)
+
 
 
 def chunks(lst, n):
     "Yield successive n-sized chunks from lst"
     list = []
-    for i in range(0, len(lst),n):
-        end = min(i+n,len(lst))
+    for i in range(0, len(lst), n):
+        end = min(i+n, len(lst))
         list.append(lst[i:end])
     return list
 
 
+filename = input("Enter Filename: ")
+filesize = os.path.getsize(filename)
+data = (filename, filesize)
+
+r = RUDP("127.0.0.1", 9001, False)
+r.connect_sock("127.0.0.1", 9002)
 r.listen()
-with open("a.txt","r") as f:
+
+r.send(data)
+with open(filename, "rb") as f:
     contents = f.read()
     print(contents)
-    list1 = chunks(contents,2048)
+    list1 = chunks(contents, 2048)
     for chunk in list1:
-        print("chunk from client :",chunk)
+        print("chunk from client :", chunk)
         r.send(chunk)
+
--- rudp/server/server.py
+++ rudp/server/server.py
@@ -4,10 +4,15 @@ from protocol_files.rudp_copy_protocol import RUDP
 import pickle
 r = RUDP("127.0.0.1",9002,True)
 r.connect_sock("127.0.0.1",9001)
-with open("acopy.txt","w") as f:
+r.listen()
+data = r.recv()
+filename, filesize = data["data"]   
+print("Receiving ", filename, filesize)
+recv_size = 0
+with open(filename,"wb") as f:
     while True:
-        # data = pickle.loads(data)
-        #print("Printing from server program :",data)
-        r.listen()
         data = r.recv()
+        recv_size += len(data["data"])
         f.write(data["data"])    
+        if(recv_size>=filesize):
+            break
