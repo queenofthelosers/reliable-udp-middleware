1736b6b84474b072e03fcb1b8b4804a7d3ec14c2
TheAlchemist1307 <nishitrchouhan@gmail.com>
2021-04-18 23:42:18 +0530

    changes

--- rudp/script.sh
+++ rudp/script.sh
@@ -2,12 +2,12 @@
 
 for i in {10..90..10}
 do
-   tc qdisc add dev lo root netem loss $i%
+   sudo tc qdisc add dev lo root netem loss $i%
    wait
    export currI=$i
    wait
    parallel -u ::: './script2.sh' './script1.sh' 
    wait
-   tc qdisc del dev lo root netem loss $i%
+   sudo tc qdisc del dev lo root netem loss $i%
    wait
 done
